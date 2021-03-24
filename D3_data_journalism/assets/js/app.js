90ddac038183ef861abfd2290843e36a48e80c34
kate <kate_spitzer@yahoo.com>
2021-03-23 20:29:53 -0700

    recommitting last changes

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -237,8 +237,8 @@ d3.csv("./assets/data/data.csv").then(function(censusData) {
 		data.age            = +data.age;
 		data.income         = +data.income;
 		data.healthcare     = +data.healthcare;
-		data.smokes         = +data.smokes;
 		data.obesity        = +data.obesity;
+		data.smokes         = +data.smokes;
 	});
 
 	// create linear scale functions
